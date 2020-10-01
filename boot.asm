6b070e7e5160eed2bca543ed2aeed401a790ed75
Fabrizio Alvarado Barquero <44043200+faoalvarado5@users.noreply.github.com>
2020-09-30 19:59:32 -0600

    update de ultima version de boot.asm

--- boot.asm
+++ boot.asm
@@ -7,7 +7,7 @@ start:
 	mov ah, 0
 	int 10h
 	
-	mov ah, 06h    ; funcion de scroll down
+	mov ah, 06h    ; funcion de scroll up para pintar el fondo verde
 	mov al, 00h     ; limpia la pantalla
 	xor cx, cx     ; esquina superior izquierda ch=linea, cl=columna
 	mov dx, 184FH  ; esquina inferior izquierda dh=lina, dl=columna 
@@ -15,7 +15,7 @@ start:
 	int 10h
 	
 	mov dh, 1 ;setea el cursor en la linea 1
-	mov dl, 5 ;setea el cursor en la columna 1
+	mov dl, 5 ;setea el cursor en la columna 5
 	mov bh, 0 ;setea el cursor en la pagina 0
 	mov ah, 2 
 	int 10h
@@ -24,15 +24,65 @@ start:
 	
 code:
 	
-	mov ah, 01h
+	mov ah, 01h ;verifica si hay keystroke en el buffer
 	int 16h
-	jnz _inputcharacter
+	jnz _inputcharacter ;brinca en caso de de zero flag sea 0
 	
 	call _timer
 	
-	call _scrollDown
+	call _printcharacterDefault
 	
 	jmp code
+	
+_printcharacterDefault:
+
+	call _imprimirDefault
+
+	mov dh, 1 ;setea el cursor en la linea 1
+	mov dl, 5 ;setea el cursor en la columna 5
+	mov bh, 0 ;setea el cursor en la pagina 0
+	mov ah, 2 
+	int 10h
+	
+	call _imprimirDefault
+	
+	mov dh, 2 ;setea el cursor en la linea 2
+	mov dl, 15 ;setea el cursor en la columna 15
+	mov bh, 0 ;setea el cursor en la pagina 0
+	mov ah, 2 
+	int 10h
+	
+	call _imprimirDefault
+	
+	mov dh, 1 ;setea el cursor en la linea 1
+	mov dl, 25 ;setea el cursor en la columna 25
+	mov bh, 0 ;setea el cursor en la pagina 0
+	mov ah, 2 
+	int 10h
+	
+	call _imprimirDefault
+	
+	mov dh, 2 ;setea el cursor en la linea 2
+	mov dl, 35 ;setea el cursor en la columna 35
+	mov bh, 0 ;setea el cursor en la pagina 0
+	mov ah, 2 
+	int 10h
+	
+	call _imprimirDefault
+	
+	call _scrollDown
+	
+	call code	
+
+_imprimirDefault:
+	
+	mov al, 1 ;caracter default es 1 en ascii, el cual es una carita feliz
+	mov bh, 0 ;imprime en la pagina 0
+	mov cx, 1 ; imprime 1 vez el caracter en al
+	mov ah, 09h ;
+	mov bl, 0x0F ;imprime de color negro el caracter.
+	int 10h
+	ret
 	
 _printcharacter:
 
@@ -46,8 +96,8 @@ _printcharacter:
 	
 	call _imprimir
 	
-	mov dh, 2 ;setea el cursor en la linea 1
-	mov dl, 15 ;setea el cursor en la columna 10
+	mov dh, 2 ;setea el cursor en la linea 2
+	mov dl, 15 ;setea el cursor en la columna 15
 	mov bh, 0 ;setea el cursor en la pagina 0
 	mov ah, 2 
 	int 10h
@@ -55,15 +105,15 @@ _printcharacter:
 	call _imprimir
 	
 	mov dh, 1 ;setea el cursor en la linea 1
-	mov dl, 25 ;setea el cursor en la columna 20
+	mov dl, 25 ;setea el cursor en la columna 25
 	mov bh, 0 ;setea el cursor en la pagina 0
 	mov ah, 2 
 	int 10h
 	
 	call _imprimir
 	
-	mov dh, 2 ;setea el cursor en la linea 1
-	mov dl, 35 ;setea el cursor en la columna 30
+	mov dh, 2 ;setea el cursor en la linea 2
+	mov dl, 35 ;setea el cursor en la columna 35
 	mov bh, 0 ;setea el cursor en la pagina 0
 	mov ah, 2 
 	int 10h
@@ -81,20 +131,18 @@ _inputcharacter:
 	
 	call _printcharacter
 	
-	ret
-	
 _timer:
 	
 	mov cx, 20h ;intervalo de tiempo
 	mov dx, 968h ; intervalo de tiempo
-	mov ah, 86h
+	mov ah, 86h ; aprox 2 segundos
 	int 15h
-	ret
-		
+	ret	
+
 _scrollDown:
 
 	mov ah, 07h    ; funcion de scroll down
-	mov al, 3     ; limpia la pantalla
+	mov al, 3     ; linea que baja el scroll
 	xor cx, cx     ; esquina superior izquierda ch=linea, cl=columna
 	mov dx, 184FH  ; esquina inferior derecha dh=lina, dl=columna 
 	mov bh, 0x02    ; color verde
